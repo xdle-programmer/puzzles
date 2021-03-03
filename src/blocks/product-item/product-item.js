5b6db419dea64ae28332d754e265ee34517abb65
Sergey <act1987@mail.ru>
2021-03-03 13:15:59 +0200

    Product slider & responsive

--- src/blocks/product-item/product-item.js
+++ src/blocks/product-item/product-item.js
@@ -1,14 +1,25 @@
 import { tns } from 'tiny-slider/src/tiny-slider'
 
-let slider = tns({
-    container: '.product-item__slider',
-    items: 1,
-    slideBy: 'page',
-    nav: false,
-    mouseDrag: true,
-    speed: 400,
-    controls: true,
-    navPosition: "bottom",
-    controlsContainer: ".product-item__slider-navigate"
-    // navContainer: ".product-item__slider-navigate"
-});
+const sliderProduct = () => {
+    let productItem = document.querySelectorAll('.product-item__slider');
+    productItem.forEach(index => {
+        let productItemNav = index.parentNode.querySelector('.product-item__slider-navigate');
+        let slider = tns({
+            container: index,
+            items: 1,
+            slideBy: 'page',
+            nav: true,
+            mouseDrag: true,
+            speed: 400,
+            controls: true,
+            navPosition: "bottom",
+            // mode: 'gallery',
+            // animateIn: "fadeIn",
+            // animateOut: "fadeOut",
+            // animateNormal: "tns-fadeOut",
+            controlsContainer: productItemNav
+        });
+    });
+}
+
+sliderProduct();
