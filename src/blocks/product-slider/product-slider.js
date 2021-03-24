b055d6b396dfe1e0c5bd17f945b6b47fa42c3f54
Устинов Юрий <yury.inform@gmail.com>
2021-03-24 10:40:54 +0200

    Пробные изменения по слайдеру

--- src/blocks/product-slider/product-slider.js
+++ src/blocks/product-slider/product-slider.js
@@ -1,45 +1,57 @@
-import { tns } from 'tiny-slider/src/tiny-slider'
+import {tns} from 'tiny-slider/src/tiny-slider';
 
-let sliderProduct = document.querySelector('.product-slider');
-if(sliderProduct){
+let sliderProduct = document.querySelector('.product-slider__thumbnails');
+if (sliderProduct) {
     initSliderProduct();
-    console.log(159)
+    console.log(159);
 }
+
 function initSliderProduct() {
-    let slider = tns({
-        container: sliderProduct,
-        items: 1,
-        controlsContainer: ".product-slider__thumbnails-arrow",
-        navContainer: ".product-slider__thumbnails",
-        navAsThumbnails: true,
-        // autoplayButton: "#customize-toggle",
-        // swipeAngle: false,
-        speed: 400
-        // slideBy: 'page',
+    // let slider = tns({
+    //     container: sliderProduct,
+    //     items: 1,
+    //     controlsContainer: ".product-slider__thumbnails-arrow",
+    //     navContainer: ".product-slider__thumbnails",
+    //     navAsThumbnails: true,
+    //     // autoplayButton: "#customize-toggle",
+    //     // swipeAngle: false,
+    //     speed: 400
+    //     // slideBy: 'page',
+    //     // nav: true,
+    //     // mouseDrag: true,
+    //     // // autoplay: true,
+    //     // autoplayButtonOutput: false,
+    //     // speed: 500,
+    //     // controls: false,
+    //     // navPosition: "bottom",
+    //     // navContainer: ".product-slider__thumbnails",
+    //     // controlsContainer: ".product-slider__thumbnails-arrow",
+    //     // navAsThumbnails: true,
+    // });
+
+    let contTest = document.getElementsByClassName('product-slider__thumbnails-arrows')[0]
+
+    console.log(contTest)
+
+    let slider2 = tns({
+        container: ".product-slider__thumbnails",
+        items: 7,
+        slideBy: 1,
+        loop: true,
         // nav: true,
-        // mouseDrag: true,
-        // // autoplay: true,
+        mouseDrag: true,
+        gutter: 10,
+        // autoplay: true,
         // autoplayButtonOutput: false,
-        // speed: 500,
+        speed: 500,
         // controls: false,
+        controlsContainer: contTest,
         // navPosition: "bottom",
-        // navContainer: ".product-slider__thumbnails",
-        // controlsContainer: ".product-slider__thumbnails-arrow",
-        // navAsThumbnails: true,
+        // nextButton: document.getElementById('123')
     });
 
-    // let slider2 = tns({
-    //     container: ".product-slider__thumbnails",
-    //     items: 3,
-    //     slideBy: 'page',
-    //     nav: true,
-    //     mouseDrag: false,
-    //     gutter: 10,
-    //     // autoplay: true,
-    //     autoplayButtonOutput: false,
-    //     speed: 500,
-    //     controls: false,
-    //     // navPosition: "bottom",
-    //     navContainer: ".product-slider__thumbnails-arrow"
+    // document.getElementById('123').addEventListener('click', () => {
+    //     slider2.goTo('next');
     // });
+
 }
